e026919f984188abd11ff57ff2b6134351bdc1d9
Basia <bachna22@gmail.com>
2019-02-07 00:03:04 +0100

    Added background to VBox in enemyInfo

--- src/main/java/com/codecool/gochiarena/view/BattleStage.java
+++ src/main/java/com/codecool/gochiarena/view/BattleStage.java
@@ -2,7 +2,10 @@ package com.codecool.gochiarena.view;
 
 import com.codecool.gochiarena.model.Gotchi;
 import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 public class BattleStage extends BorderPane {
 
@@ -13,17 +16,20 @@ public class BattleStage extends BorderPane {
 
     public BattleStage() {
         this.setPadding(new Insets(10));
+        this.actionChoose.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
         this.setLeft(actionChoose);
         this.setBottom(battleMessageView);
     }
 
     public void setGotchiInfo(Gotchi gotchi) {
         this.playerInfo = new GotchiInfo(gotchi);
+        this.playerInfo.setBackground(new Background(new BackgroundFill(Color.GREENYELLOW, null, null)));
         this.setTop(playerInfo);
     }
 
     public void setEnemyInfo(Gotchi gotchi) {
         this.enemyInfo = new EnemyGotchisInfo(gotchi);
+        this.enemyInfo.setBackground(new Background(new BackgroundFill(Color.LIGHTGRAY, null, null)));
         this.setRight(enemyInfo);
     }
 
