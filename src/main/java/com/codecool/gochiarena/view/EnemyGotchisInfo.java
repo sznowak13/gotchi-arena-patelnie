f0352f43c07f3d8fe0a19cbb3794f81c12bed2b4
Szymon Nowak <sznowak13@gmail.com>
2019-02-06 18:01:05 +0100

    Created new class EnemyGotchisInfo

--- src/main/java/com/codecool/gochiarena/view/EnemyGotchisInfo.java
+++ src/main/java/com/codecool/gochiarena/view/EnemyGotchisInfo.java
@@ -1,4 +1,11 @@
 package com.codecool.gochiarena.view;
 
-public class EnemyGotchisInfo {
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class EnemyGotchisInfo extends VBox {
+    private Text enemyName = new Text();
+    private Text name = new Text();
+    private Text health = new Text();
+    private Text stamina = new Text();
 }
