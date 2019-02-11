a74c2e34765b66cb2e5d333a87b714abd03f70dd
Basia <bachna22@gmail.com>
2019-02-11 15:27:15 +0100

    Changed view of PrepareBattle, added Vbox with images in BorderPane Center, and buttons to BorderPane Bottom

--- src/main/java/com/codecool/gochiarena/view/PrepareBattle.java
+++ src/main/java/com/codecool/gochiarena/view/PrepareBattle.java
@@ -1,14 +1,14 @@
 package com.codecool.gochiarena.view;
 
+import com.codecool.gochiarena.model.GochiType;
 import com.codecool.gochiarena.model.Gotchi;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -22,18 +22,23 @@ public class PrepareBattle extends BorderPane {
     public PrepareBattle() {
         this.createGotchiList();
         this.setLeft(this.gotchiListing);
-        this.setCenter(this.selectGotchi);
-        this.setBottom(this.backButton);
+        this.setRight(this.selectGotchi);
+        this.setBottom(this.createHorizontalFieldForButtons());
     }
 
     public void createGotchiList() {
         this.gotchiListing.getChildren().clear();
-        this.gotchiListing.setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
+        this.gotchiListing.setBackground(new Background(new BackgroundFill(Color.LIGHTSEAGREEN, null, null)));
+        this.gotchiListing.setPadding(new Insets(0, 15, 0, 15));
+        this.gotchiListing.setAlignment(Pos.CENTER_LEFT);
         this.gotchiListing.getChildren().add(new Text("Available Gotchis: "));
         for (Gotchi gotchi : Gotchi.getAvailableGotchis()) {
             RadioButton listItem = new RadioButton(gotchi.getName() + " " + gotchi.getType());
             listItem.setUserData(gotchi);
             listItem.setToggleGroup(this.gotchiToggle);
+            listItem.setOnMouseClicked(event -> {
+                this.setCenter(createVerticalFieldForType(TypeImage.valueOf(gotchi.getType().toString()).id));
+            });
             this.gotchiListing.getChildren().add(listItem);
         }
         this.gotchiToggle.selectToggle(this.gotchiToggle.getToggles().get(0));
@@ -53,9 +58,38 @@ public class PrepareBattle extends BorderPane {
         });
     }
 
-    public void setupBackButton(Stage primaryStage, Scene scene){
+    public void setupBackButton(Stage primaryStage, Scene scene) {
         this.backButton.setOnAction((event) -> {
             primaryStage.setScene(scene);
         });
+    }
+
+    private HBox createHorizontalFieldForButtons(){
+        HBox horizontalFieldForButtons = new HBox();
+        horizontalFieldForButtons.setBackground(new Background(new BackgroundFill(Color.LIGHTSEAGREEN, null, null)));
+        horizontalFieldForButtons.setPadding(new Insets(0, 0, 0, 2)); // <----- looks like magic number and it is
+        horizontalFieldForButtons.setSpacing(ViewConfig.WIDTH/3);
+        horizontalFieldForButtons.getChildren().addAll(backButton, selectGotchi);
+        return horizontalFieldForButtons;
+    }
+
+    private VBox createVerticalFieldForType(String id) {
+        VBox verticalField = new VBox();
+        verticalField.getStyleClass().add("vbox");
+        verticalField.setId(id);
+        return verticalField;
+    }
+
+}
+
+enum TypeImage {
+    ROCK("vbox-custom-rock"),
+    PAPER("vbox-custom-paper"),
+    SCISSORS("vbox-custom-scissors");
+
+    final String id;
+
+    private TypeImage(String id) {
+        this.id = id;
     }
 }
