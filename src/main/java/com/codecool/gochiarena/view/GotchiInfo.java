3a9be07b747b44f86f3f382afd919a5fd005a2b4
Basia <bachna22@gmail.com>
2019-02-16 11:55:16 +0100

    deleted import

--- src/main/java/com/codecool/gochiarena/view/GotchiInfo.java
+++ src/main/java/com/codecool/gochiarena/view/GotchiInfo.java
@@ -1,7 +1,6 @@
 package com.codecool.gochiarena.view;
 
 import com.codecool.gochiarena.model.Gotchi;
-import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
