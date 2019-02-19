a796ecdba7860266af515f0200a64560337ae6db
bdziedzic <bartosz.dziedzic94@gmail.com>
2019-02-19 22:55:25 +0100

    add getting random action for cpu

--- src/main/java/com/codecool/gochiarena/model/Action.java
+++ src/main/java/com/codecool/gochiarena/model/Action.java
@@ -1,8 +1,21 @@
 package com.codecool.gochiarena.model;
 
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum Action {
     PRIMARY_ATTACK,
     SECONDARY_ATTACK,
     DEFEND,
-    EVADE
+    EVADE;
+
+ public static Object getRandomAction() {
+        final List<Action> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+        Random random = new Random();
+        return VALUES.get(random.nextInt(VALUES.size()));
+    }
 }
