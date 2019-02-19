b2110ff5e435b7a8f59e6e2439f233f708d2d1ee
Szymon Nowak <sznowak13@gmail.com>
2019-02-19 22:22:50 +0100

    Removed mainPane field from BattleStage as its now can extend the pane itself

--- src/main/java/com/codecool/gochiarena/controller/BattleStageController.java
+++ src/main/java/com/codecool/gochiarena/controller/BattleStageController.java
@@ -1,12 +1,16 @@
 package com.codecool.gochiarena.controller;
 
 import com.codecool.gochiarena.model.BattleArena;
+import com.codecool.gochiarena.model.Gotchi;
+import com.codecool.gochiarena.model.GotchiDAO;
 import com.codecool.gochiarena.view.BattleStage;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.Observable;
 import java.util.Observer;
 
-public class BattleStageController implements Observer {
+public class BattleStageController implements PropertyChangeListener {
 
     private BattleStage battleStage;
     private BattleArena battleArena;
