3891520ecc644b22cf9187fad135953f9fba5e77
Basia <bachna22@gmail.com>
2019-02-20 12:26:54 +0100

    Changed naming of classes

--- src/main/java/com/codecool/gochiarena/model/BatlleArenaModel.java
+++ src/main/java/com/codecool/gochiarena/model/BatlleArenaModel.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class BattleArena {
+public class BatlleArenaModel {
 
     private List<Gotchi> gotchis = new ArrayList<>();
 
