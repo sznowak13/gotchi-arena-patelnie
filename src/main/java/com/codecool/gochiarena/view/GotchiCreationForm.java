9774807eab71503b54778ccba949118076dab1fb
Szymon Nowak <sznowak13@gmail.com>
2019-02-20 12:00:44 +0100

    Refactored adding and removing warning msg in form

--- src/main/java/com/codecool/gochiarena/view/GotchiCreationForm.java
+++ src/main/java/com/codecool/gochiarena/view/GotchiCreationForm.java
@@ -23,7 +23,7 @@ public class GotchiCreationForm extends VBox {
     private Button createGotchi = new Button("Create!");
     private Button backButton = new Button("Back to main menu");
 
-    private final Text warningMsg = new Text("Warning: You mustn't exceed 200pts");
+    private final Text warningMsg = new Text();
     private GotchiFormController controller;
     private String[] statLabels = new String[] {"Attack", "Defence", "Speed", "Stamina"};
 
@@ -42,6 +42,8 @@ public class GotchiCreationForm extends VBox {
         for (String statName : this.statLabels) {
             this.addStatField(statName);
         }
+        this.warningMsg.setId("create-gotchi-warning-msg");
+        this.getChildren().add(warningMsg);
 
 
         this.createHorizontalField();
@@ -65,7 +67,6 @@ public class GotchiCreationForm extends VBox {
             if (error == null) {
                 primaryStage.setScene(scene);}
             else {
-                this.removeExceedWarning();
                 this.addExceedWarning(error);
             }
         });
@@ -117,12 +118,10 @@ public class GotchiCreationForm extends VBox {
 
     private void addExceedWarning(String message){
         this.warningMsg.setText(message);
-        this.warningMsg.setId("create-gotchi-warning-msg");
-        this.getChildren().add(this.warningMsg);
     }
 
-    private void removeExceedWarning(){
-        this.getChildren().remove(this.warningMsg);
+    private void removeExceedWarning() {
+        this.warningMsg.setText("");
     }
 
 }
